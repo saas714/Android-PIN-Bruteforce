7c7c325364ca7ae86e75c3ff6ec5370b9c961138
saas714 <michael.diaz888s@gmail.com>
2025-03-15 08:45:12 +0000

    git

---
+++ .ipynb_checkpoints/pythondebuggy.py
@@ -0,0 +1,14 @@
+bash
+
+Process ID (PID): 3764
+
+Command line: /bin/bash
+
+Shell integration activated
+
+The following extensions have contributed to this terminal's environment:
+
+GitHub Codespaces
+Git: Enables the following features: git auth provider
+Python Debugger: Enables use of no-config debugging, debugpy <script.py>, in the terminal.
+RobotCode - Robot Framework Support
